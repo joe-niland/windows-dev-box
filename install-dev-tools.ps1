e4f90ed4c9277e372dc603934078174f56ccb9e4
Joe Niland <joe@originalmind.com.au>
2015-11-23 19:00:33 +1100

    adding vagrant

--- install-dev-tools.ps1
+++ install-dev-tools.ps1
@@ -2,4 +2,4 @@
 
 Write-Host "Installing other common development components"
 
-cinst putty heidisql winscp openssl.light psget git-credential-winstore fiddler4 Logparser Logparser.LizardGui sysinternals winmerge nmap wireshark curl paint.net
+cinst vagrant putty heidisql winscp openssl.light psget git-credential-winstore fiddler4 Logparser Logparser.LizardGui sysinternals winmerge nmap wireshark curl paint.net
