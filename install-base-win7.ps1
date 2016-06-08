2d681771fa1bf8f3cdbeb1bd145a5d4c5d486a84
Joe Niland <joe@originalmind.com.au>
2016-06-08 14:47:38 +1000

    Adding clink to base; script to overwrite Cmder's copy of clink with version from Chocolatey

--- install-base-win7.ps1
+++ install-base-win7.ps1
@@ -1,4 +1,10 @@
-cinst GoogleChrome cmder truecrypt lastpass onedrive
+cinst GoogleChrome clink cmder truecrypt lastpass onedrive
 cinst TotalCommander -version 8.52.01
 choco install gcm -pre
 cinst Powershell -pre
+
+# Update built Cmder's copy of clink with the version from Chocolatey
+$clinkDir = gci ${env:ProgramFiles(x86)}\clink | ?{ $_.PSIsContainer } | sort-object -property CreationTime -Descending | select-object -first 1 fullname
+if ($clinkDir) {
+	copy-item -recurse ($clinkDir.fullname + "\*") c:\tools\cmder\vendor\clink
+}
