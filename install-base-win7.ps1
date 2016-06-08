428fb4db089848e3b11be6e3b46720083f46d89b
Joe Niland <joe@originalmind.com.au>
2016-06-08 14:51:35 +1000

    Adding command to remove conflicting copies of clink

--- install-base-win7.ps1
+++ install-base-win7.ps1
@@ -8,3 +8,6 @@ $clinkDir = gci ${env:ProgramFiles(x86)}\clink | ?{ $_.PSIsContainer } | sort-ob
 if ($clinkDir) {
 	copy-item -recurse ($clinkDir.fullname + "\*") c:\tools\cmder\vendor\clink
 }
+
+# Now remove the Chocolatey version because it seems to conflict with Cmder's version
+cuninst clink -y
