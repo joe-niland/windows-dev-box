1fdafc53ad426fb3af99da4279759671532cfe2e
Original Mind <tech@originalmind.com.au>
2016-06-14 22:11:41 +1000

    Cleaning up install base scripts

--- install-base-win10.ps1
+++ install-base-win10.ps1
@@ -1,6 +1,13 @@
-cinst chocolatey GoogleChrome lastpass gpg4win
-cinst git -params "/GitAndUnixToolsOnPath" -y
-cinst cmder -pre -y
-cinst TotalCommander -version 8.52.01 -y
-choco install gcm -pre -y
+cinst chocolatey GoogleChrome clink cmder lastpass onedrive TotalCommander gpg4win -y
+Install-Package git-credential-manager-for-windows -confirm
 Install-Module posh-git
+
+
+# Update built Cmder's copy of clink with the version from Chocolatey
+$clinkDir = gci ${env:ProgramFiles(x86)}\clink | ?{ $_.PSIsContainer } | sort-object -property CreationTime -Descending | select-object -first 1 fullname
+if ($clinkDir) {
+	copy-item -recurse ($clinkDir.fullname + "\*") c:\tools\cmder\vendor\clink
+}
+
+# Now remove the Chocolatey version because it seems to conflict with Cmder's version
+cuninst clink -y
